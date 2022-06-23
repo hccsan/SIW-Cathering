87d81794ffa19b84af43bcad56284d241f529331
hccsan <72209607+hccsan@users.noreply.github.com>
2022-06-23 16:52:49 +0200

    initial

---
+++ siw-cathering/src/main/java/com/example/demo/SiwCatheringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwCatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwCatheringApplication.class, args);
+	}
+
+}
